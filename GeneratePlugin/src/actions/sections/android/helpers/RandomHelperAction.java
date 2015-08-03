0e6073e40d0fb19a0b11dd5863e4657bd732fb24
dlernatovich <dlernatovich@geeksforless.net>
2015-08-03 20:17:50 +0300

    Changes

--- GeneratePlugin/src/actions/sections/android/helpers/RandomHelperAction.java
+++ GeneratePlugin/src/actions/sections/android/helpers/RandomHelperAction.java
@@ -8,17 +8,37 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
  */
 public class RandomHelperAction extends BaseAction {
 
-    private final String CODE = "private static int MIN_VALUE = 0;\n" +
-            "    private static int MAX_VALUE = 900000;\n" +
+    private final String CODE = "private static final int MIN_VALUE = 0;\n" +
+            "    private static final int MAX_VALUE = 900000;\n" +
+            "    private static final String ALPHABET = \"QWERTYUIOP{}ASDFGHJKL:|ZXCVBNM<>?qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890-=+_\";\n" +
             "\n" +
             "    /**\n" +
-            "     * Method which\n" +
-            "     * @return\n" +
+            "     * Method which provide the generating of the random integer value\n" +
+            "     *\n" +
+            "     * @return current random integer value\n" +
             "     */\n" +
             "    public static int generateRandomInteger() {\n" +
             "        Random rand = new Random();\n" +
             "        int randomNum = rand.nextInt((MAX_VALUE - MIN_VALUE) + 1) + MIN_VALUE;\n" +
             "        return randomNum;\n" +
+            "    }\n" +
+            "\n" +
+            "    /**\n" +
+            "     * Method which provide the generating of the random String value with length\n" +
+            "     *\n" +
+            "     * @param length current random string length\n" +
+            "     * @return generated String value\n" +
+            "     */\n" +
+            "    public static String generateRandomString(int length) {\n" +
+            "        char[] chars = ALPHABET.toCharArray();\n" +
+            "        StringBuilder sb = new StringBuilder();\n" +
+            "        Random random = new Random();\n" +
+            "        for (int i = 0; i < length; i++) {\n" +
+            "            char c = chars[random.nextInt(chars.length)];\n" +
+            "            sb.append(c);\n" +
+            "        }\n" +
+            "        String output = sb.toString();\n" +
+            "        return output;\n" +
             "    }";
 
     @Override
